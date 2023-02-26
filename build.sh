791d6d0e1b704d6caef52fd191b140966bdfa102
anderson-cruz-fernandes <anderson-cruz-fernandes@users.noreply.github.com>
2023-02-26 17:03:40 +0000

    Melhorias na geração do arquivo main.zip

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-GOOS=linux CGO_ENABLED=0 go build ~/aws-lambda-return-from-json/cmd/main.go
-sleep 5
-zip -jrm ~/aws-lambda-return-from-json/cmd/main.zip ~/aws-lambda-return-from-json/cmd/main
-echo "INFO: Arquivo \"main.zip\" criado em ~/aws-lambda-return-from-json/cmd"
--- cmd/build.sh
+++ cmd/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-[ -e ~/aws-lambda-return-from-json/cmd/main.zip ] && rm ~/aws-lambda-return-from-json/cmd/main.zip
+cd ~/aws-lambda-return-from-json/cmd
 GOOS=linux CGO_ENABLED=0 go build ~/aws-lambda-return-from-json/cmd/main.go
-sleep 10
-zip -jrm ~/aws-lambda-return-from-json/cmd/main.zip ~/aws-lambda-return-from-json/cmd/main
+sleep 5
+[ -e ~/aws-lambda-return-from-json/cmd/main.zip ] && rm ~/aws-lambda-return-from-json/cmd/main.zip
+zip -jrm ~/aws-lambda-return-from-json/cmd/main.zip ~/aws-lambda-return-from-json/cmd/main && echo "INFO: Arquivo \"main.zip\" criado em ~/aws-lambda-return-from-json/cmd"
