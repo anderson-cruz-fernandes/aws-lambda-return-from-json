1b01533de799bad96eca60b22a9c580b64fc9b33
anderson-cruz-fernandes <anderson-cruz-fernandes@users.noreply.github.com>
2023-02-26 16:41:46 +0000

    Ajuste no script de build para remover o .zip antes de criá-lo Aumentado o tempo de espera para 10 segundos

--- cmd/build.sh
+++ cmd/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+[ -e ~/aws-lambda-return-from-json/cmd/main.zip ] && rm ~/aws-lambda-return-from-json/cmd/main.zip
 GOOS=linux CGO_ENABLED=0 go build ~/aws-lambda-return-from-json/cmd/main.go
-sleep 5
+sleep 10
 zip -jrm ~/aws-lambda-return-from-json/cmd/main.zip ~/aws-lambda-return-from-json/cmd/main
